3c37f66558a92df8278187c34f04ccdd44ce8c5b
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-13 21:32:21 +0530

    Add files via upload

---
+++ for_Loop.py
@@ -0,0 +1,37 @@
+# a=1,2,3,4,5,6,7,8,9
+# print(a)
+# print(*a)
+
+# a=1,2,3,4
+# for i in a:
+#     print(i,end=" ")
+
+# for i in range(5):
+#     print(i)
+
+# for a in range(20):
+#     print(a)
+
+# n=int(input("Enter the value of n:"))
+# for i in range(1,n+1):
+#     print(i,end="\n")
+
+# for i in range(1,30,2):
+#     print(i,end=' ')
+
+# for i in range(2,30,2):
+#     print(i,end=' ')
+
+# for i in range(10,0,-1):
+#     print(i,end=' ')
+
+# for i in range(10,-1,-1):
+#     print(i,end=' ')
+
+# n=[1,4.5,'python']
+# for i in n[::-1]:
+#     print(i)
+
+# v='program'
+# for i in v[::2]:
+#     print(i)
---
+++ for_loop_part2.py
@@ -0,0 +1,36 @@
+# n=int(input("Enter any Integer value:"))
+# for i in range(n):
+#     print(i)
+#     print(i)
+#     print(i)
+#     print("The loop has been executed.")
+# print("Done")
+
+# n=int(input("Enter any integer value:"))
+# for i in range(1,n):
+#     print(i)
+
+# n=int(input("Enter any integer value:"))
+# for i in range(25,n):
+#     print(i,end=" ")
+
+# n=int(input("Enter the integer value:"))
+# for i in range(2,n,2):
+#     print(i,end=" ")
+
+# n=int(input("Enter any integer value:"))
+# if(n%2==0):
+#     print(f"{n} is an even number")
+# else:
+#     print(f"{n} is an odd number")
+
+# for i in range(1,5):
+#     print(i*i)
+
+# for i in range(1,5):
+#     print(i*i*i)
+
+# n=int(input("Enter the value of n:"))
+# while(n<=10):
+#     print("hello",str(n))
+#     n+=1
